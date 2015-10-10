a7aaf36792850e48dd9e1526c53c6b603a312a9b
Andy Krause <andy.krause@unimelb.edu.au>
2015-10-11 10:02:35 +1100

    added sens anlys and raw Res export
    Added sensitivity analysis for model as well as outputting of raw
    results

--- akModelComparisonAnalysis/prmcDataAnalysis.R
+++ akModelComparisonAnalysis/prmcDataAnalysis.R
@@ -9,7 +9,8 @@
   ## Set parameters
 
   reBuildData <- FALSE     # Enter TRUE if you wish to rebuild data from raw
-
+  testCRMSens <- FALSE     # Test the sensitivity of the result to alternate
+                             # regression models?
   ## Load Libraries
 
   library(plyr)
@@ -35,7 +36,7 @@
   sla1GeoFile <- 'Vic_SLA1.shp'
   postGeoFile <- 'Vic_PostCodes.shp'
 
-### Read in data ---------------------------------------------------------------
+### Read in and prepare data ---------------------------------------------------------------
   
  ## Read in transaction Data  
   
@@ -54,51 +55,138 @@
   sla1Shp <- readShapePoly(paste0(dataPath, sla1GeoFile))
   postCodeShp <- readShapePoly(paste0(dataPath, postGeoFile))
 
+### Cross regression comparison method -----------------------------------------
 
+ ## Set the specification (formula)
 
+ regSpec <- log(transValue) ~ log(AreaSize) + Bedrooms + Baths + 
+    as.factor(postCode) + as.factor(transQtr)
 
+ ## Estimate models and make new predictions: Global by Use
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Develop the cross regression comparison method -------------------------------------------------
-
-## Set the specification (formula)
-
-regSpec <- log(transValue) ~ log(AreaSize) + Bedrooms + Baths + as.factor(Suburb) +
-  as.factor(transQtr)
-
-## Estimate models and make new predictions
-
-houseResults <- prrCrossReg(regSpec, 
-                            subset(xSales, PropertyType == 'House'),
-                            subset(xRentals, PropertyType == 'House'),
+  # For houses
+  houseResults <- prrCrossReg(regSpec, 
+                            subset(allTrans, transType == 'sale' &
+                                             PropertyType == 'House' & 
+                                             YT_house_postCode == 1),
+                            subset(allTrans, transType == 'rent' &
+                                             PropertyType == 'House' & 
+                                             YT_house_postCode == 1),
                             verbose=TRUE)
-
-unitResults <- prrCrossReg(regSpec, 
-                           subset(xSales, PropertyType == 'Unit'),
-                           subset(xRentals, PropertyType == 'Unit'),
+  
+  # For Units
+  unitResults <- prrCrossReg(regSpec, 
+                           subset(allTrans, transType == 'sale' &
+                                    PropertyType == 'Unit' & 
+                                    YT_unit_postCode == 1),
+                           subset(allTrans, transType == 'rent' &
+                                    PropertyType == 'Unit' & 
+                                    YT_unit_postCode == 1),
                            verbose=TRUE)
 
-## Calculate the ratio
+ ## Calculate the ratio
 
-# Extract vales
-allValues <- rbind(houseResults$allData, unitResults$allData)
+  # Extract vales
+  crmValues <- rbind(houseResults, unitResults)
 
-# Calculate the ratio
-allValues$prRatio <- allValues$Price / (allValues$Rent * 52 / 12)
+  # Calculate the ratio
+  crmValues$prRatio <- crmValues$Price / (crmValues$Rent * 52 / 12)
 
-### Clean up memory
+ ## Calculate sensitivity to the model
+  
+ if(testCRMSpec){
+  
+  # Specify two alternative models
+  
+  altSpec1 <- log(transValue) ~ Bedrooms + 
+    as.factor(postCode) + as.factor(transYear)
+  
+  altSpec2 <- log(transValue) ~ log(AreaSize) + Bedrooms + Baths + 
+    HasPool + HasGarage + HasAirConditioning + HasFireplace +
+    as.factor(postCode) + as.factor(transMonth)
+  
+  # For houses
+  houseResultsA1 <- prrCrossReg(altSpec1, 
+                              subset(allTrans, transType == 'sale' &
+                                       PropertyType == 'House' & 
+                                       YT_house_postCode == 1),
+                              subset(allTrans, transType == 'rent' &
+                                       PropertyType == 'House' & 
+                                       YT_house_postCode == 1),
+                              verbose=TRUE)
+  houseResultsA2 <- prrCrossReg(altSpec2, 
+                              subset(allTrans, transType == 'sale' &
+                                       PropertyType == 'House' & 
+                                       YT_house_postCode == 1),
+                              subset(allTrans, transType == 'rent' &
+                                       PropertyType == 'House' & 
+                                       YT_house_postCode == 1),
+                              verbose=TRUE)
+  
+  # For Units
+  unitResultsA1 <- prrCrossReg(altSpec1, 
+                             subset(allTrans, transType == 'sale' &
+                                      PropertyType == 'Unit' & 
+                                      YT_unit_postCode == 1),
+                             subset(allTrans, transType == 'rent' &
+                                      PropertyType == 'Unit' & 
+                                      YT_unit_postCode == 1),
+                             verbose=TRUE)
+  unitResultsA2 <- prrCrossReg(altSpec2, 
+                             subset(allTrans, transType == 'sale' &
+                                      PropertyType == 'Unit' & 
+                                      YT_unit_postCode == 1),
+                             subset(allTrans, transType == 'rent' &
+                                      PropertyType == 'Unit' & 
+                                      YT_unit_postCode == 1),
+                             verbose=TRUE)
+  
+  # Extract vales
+  crmAltValues1 <- rbind(houseResultsA1, unitResultsA1)
+  crmAltValues2 <- rbind(houseResultsA2, unitResultsA2)
+  
+  # Calculate the ratio
+  crmAltValues1$prRatio <- crmAltValues1$Price / (crmAltValues1$Rent * 52 / 12)
+  crmAltValues2$prRatio <- crmAltValues2$Price / (crmAltValues2$Rent * 52 / 12)
+  
+  a1Diff <- crmValues$prRatio - crmAltValues1$prRatio
+  a2Diff <- crmValues$prRatio - crmAltValues2$prRatio
+  
+  summary(a1Diff)
+  summary(a2Diff)
+  
+ ## Make Comparisons
+  
+  # Add values to full dataset
+  allTrans$prRatio <- crmValues$prRatio[match(allTrans$UID, crmValues$UID)]
+  xTrans <- subset(allTrans, !is.na(prRatio))
+  xTrans$pr1 <- crmAltValues1$prRatio[match(xTrans$UID, crmAltValues1$UID)]
+  xTrans$pr2 <- crmAltValues2$prRatio[match(xTrans$UID, crmAltValues2$UID)]
+  
+  # Make Comparison plots
+  plot(tapply(xTrans$prRatio, xTrans$transQtr, median), type='l', col=2, lwd=3,
+       xlab='Quarter', ylab='PRR')
+  lines(tapply(xTrans$pr1, xTrans$transQtr, median), type='l', col=1, lwd=1)
+  lines(tapply(xTrans$pr2, xTrans$transQtr, median), type='l', col=4, lwd=1)
+  legend('bottomleft', c("Main", 'Alt1', 'Alt2'), col=c(2,1,4), lwd=c(3,1,1))
+  
+ ## Clean up memory
+  rm(unitResultsA1, unitResultsA2, houseResultsA1, houseResultsA2)
+  rm(crmAltValues1, crmAltValues2); gc()
+  
+ } else {
+ 
+   allTrans$prRatio <- crmValues$prRatio[match(allTrans$UID, crmValues$UID)]
+   xTrans <- subset(allTrans, !is.na(prRatio)) 
+       
+ }
 
-rm(rentals); rm(sales); rm(xSales); rm(xRentals)
-gc()
+### Output raw results ---------------------------------------------------------
+  
+  write.csv(xTrans, paste0(dataPath, 'rawResults.csv'), row.names=FALSE)
+    
+### Break down results by dimensions -------------------------------------------
+  
+  
+  
+  

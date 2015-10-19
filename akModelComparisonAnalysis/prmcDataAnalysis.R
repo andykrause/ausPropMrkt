cdd134daad47eb627d8744ace3262d9d746a10fe
Andy Krause <andy.krause@unimelb.edu.au>
2015-10-20 09:55:49 +1100

    updated analysis for Paper

--- akModelComparisonAnalysis/prmcDataAnalysis.R
+++ akModelComparisonAnalysis/prmcDataAnalysis.R
@@ -9,9 +9,8 @@
   ## Set parameters
 
   reBuildData <- FALSE     # Enter TRUE if you wish to rebuild data from raw
-  testCRMSens <- FALSE     # Test the sensitivity of the result to alternate
-                             # regression models?
-  ## Load Libraries
+
+ ## Load Libraries
 
   library(plyr)
   library(dplyr)
@@ -55,12 +54,23 @@
   sla1Shp <- readShapePoly(paste0(dataPath, sla1GeoFile))
   postCodeShp <- readShapePoly(paste0(dataPath, postGeoFile))
 
+### Median method approach
+  
+ ## Global Analysis
+  
+  mmGlobY <- prrMedMethod(allTrans)
+  mmGlobQ <- prrMedMethod(allTrans, 'transQtr')
+  
+  mmGlobBUY <- prrMedMethod(allTrans, 'transYear', TRUE)
+  
+  
+
 ### Cross regression comparison method -----------------------------------------
 
  ## Set the specification (formula)
 
- regSpec <- log(transValue) ~ log(AreaSize) + Bedrooms + Baths + 
-    as.factor(postCode) + as.factor(transQtr) + ssInteg + ssChoice
+  regSpec <- log(transValue) ~ as.factor(postCode) + as.factor(transQtr) + 
+                log(AreaSize) +Bedrooms + Baths + ssInteg + ssChoice
 
  ## Estimate models and make new predictions: Global by Use
 
@@ -92,95 +102,10 @@
   # Calculate the ratio
   crmValues$prRatio <- crmValues$Price / (crmValues$Rent * 52 / 12)
 
- ## Calculate sensitivity to the model
-  
- if(testCRMSens){
-  
-  # Specify two alternative models
-  
-  altSpec1 <- log(transValue) ~ Bedrooms + ssInteg + ssChoice +
-     as.factor(transMonth) 
-  
-  altSpec2 <- log(transValue) ~ log(AreaSize) + Bedrooms + Baths + 
-    HasPool + HasGarage + HasAirConditioning + HasFireplace +
-    as.factor(postCode) + as.factor(transMonth) 
-  
-  # For houses
-  houseResultsA1 <- prrCrossReg(altSpec1, 
-                              subset(allTrans, transType == 'sale' &
-                                       PropertyType == 'House' & 
-                                       YT_house_postCode == 1),
-                              subset(allTrans, transType == 'rent' &
-                                       PropertyType == 'House' & 
-                                       YT_house_postCode == 1),
-                              verbose=TRUE)
-  houseResultsA2 <- prrCrossReg(altSpec2, 
-                              subset(allTrans, transType == 'sale' &
-                                       PropertyType == 'House' & 
-                                       YT_house_postCode == 1),
-                              subset(allTrans, transType == 'rent' &
-                                       PropertyType == 'House' & 
-                                       YT_house_postCode == 1),
-                              verbose=TRUE)
-  
-  # For Units
-  unitResultsA1 <- prrCrossReg(altSpec1, 
-                             subset(allTrans, transType == 'sale' &
-                                      PropertyType == 'Unit' & 
-                                      YT_unit_postCode == 1),
-                             subset(allTrans, transType == 'rent' &
-                                      PropertyType == 'Unit' & 
-                                      YT_unit_postCode == 1),
-                             verbose=TRUE)
-  unitResultsA2 <- prrCrossReg(altSpec2, 
-                             subset(allTrans, transType == 'sale' &
-                                      PropertyType == 'Unit' & 
-                                      YT_unit_postCode == 1),
-                             subset(allTrans, transType == 'rent' &
-                                      PropertyType == 'Unit' & 
-                                      YT_unit_postCode == 1),
-                             verbose=TRUE)
-  
-  # Extract vales
-  crmAltValues1 <- rbind(houseResultsA1$results, unitResultsA1$results)
-  crmAltValues2 <- rbind(houseResultsA2$results, unitResultsA2$results)
-  
-  # Calculate the ratio
-  crmAltValues1$prRatio <- crmAltValues1$Price / (crmAltValues1$Rent * 52 / 12)
-  crmAltValues2$prRatio <- crmAltValues2$Price / (crmAltValues2$Rent * 52 / 12)
-  
-  a1Diff <- crmValues$prRatio - crmAltValues1$prRatio
-  a2Diff <- crmValues$prRatio - crmAltValues2$prRatio
-  
-  summary(a1Diff)
-  summary(a2Diff)
-  
- ## Make Comparisons
-  
-  # Add values to full dataset
+  # Add Ratio to full dataset 
   allTrans$prRatio <- crmValues$prRatio[match(allTrans$UID, crmValues$UID)]
-  xTrans <- subset(allTrans, !is.na(prRatio))
-  xTrans$pr1 <- crmAltValues1$prRatio[match(xTrans$UID, crmAltValues1$UID)]
-  xTrans$pr2 <- crmAltValues2$prRatio[match(xTrans$UID, crmAltValues2$UID)]
-  
-  # Make Comparison plots
-  plot(tapply(xTrans$prRatio, xTrans$transQtr, median), type='l', col=2, lwd=3,
-       xlab='Quarter', ylab='PRR')
-  lines(tapply(xTrans$pr1, xTrans$transQtr, median), type='l', col=1, lwd=1)
-  lines(tapply(xTrans$pr2, xTrans$transQtr, median), type='l', col=4, lwd=1)
-  legend('bottomleft', c("Main", 'Alt1', 'Alt2'), col=c(2,1,4), lwd=c(3,1,1))
-  
- ## Clean up memory
-  rm(unitResultsA1, unitResultsA2, houseResultsA1, houseResultsA2)
-  rm(crmAltValues1, crmAltValues2); gc()
-  
- } else {
- 
-   allTrans$prRatio <- crmValues$prRatio[match(allTrans$UID, crmValues$UID)]
-   xTrans <- subset(allTrans, !is.na(prRatio)) 
+  xTrans <- subset(allTrans, !is.na(prRatio)) 
        
- }
-
 ### Output raw results ---------------------------------------------------------
   
   write.csv(xTrans, paste0(dataPath, 'rawResults.csv'), row.names=FALSE)
@@ -210,19 +135,19 @@
  ## All lgas 
   
   # Basic
- lgaY <- prrTrender('transYear', xData=xTrans, geog='lga', geogName='all')
- lgaQ <- prrTrender('transQtr', xData=xTrans, geog='lga', geogName='all')
+  lgaY <- prrTrender('transYear', xData=xTrans, geog='lga', geogName='all')
+  lgaQ <- prrTrender('transQtr', xData=xTrans, geog='lga', geogName='all')
   
   # By Use
- lgaYU <- prrTrender('transYear', xData=xTrans, geog='lga', 
+  lgaYU <- prrTrender('transYear', xData=xTrans, geog='lga', 
                       geogName='all', byUse=TRUE)
- lgaQU <- prrTrender('transQtr', xData=xTrans, geog='lga', 
+  lgaQU <- prrTrender('transQtr', xData=xTrans, geog='lga', 
                       geogName='all', byUse=TRUE)
   
   # By use weighted
- lgaYUW <- prrTrender('transYear', xData=xTrans, geog='lga',
+  lgaYUW <- prrTrender('transYear', xData=xTrans, geog='lga',
                        geogName='all', byUse=TRUE, weighted=TRUE)
- lgaQUW <- prrTrender('transQtr', xData=xTrans, geog='lga',
+  lgaQUW <- prrTrender('transQtr', xData=xTrans, geog='lga',
                        geogName='all', byUse=TRUE, weighted=TRUE)
   
  ## All SLA1s 
@@ -287,7 +212,7 @@
       slaY, slaQ, slaYU, slaQU, slaYUW, slaQUW,
       pcY, pcQ, pcYU, pcQU, pcYUW, pcQUW,
       subY, subQ, subYU, subQU, subYUW, subQUW,
-      subShp, lgaShp, sla1Shp, postCodeShp,
+      subShp, lgaShp, sla1Shp, postCodeShp, xTrans,
       file = paste0(dataPath, 'plotObjs.rData'))
   
 ### Visualize Results ----------------------------------------------------------  

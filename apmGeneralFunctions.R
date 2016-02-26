0183f92f05b4f7c6e2df505a465536297a6e8904
Andy Krause <andy.krause@unimelb.edu.au>
2016-02-26 12:47:15 +1100

    initial commit of general functions

---
+++ apmGeneralFunctions.R
@@ -0,0 +1,28 @@
+##########################################################################################
+#                                                                                        #
+#   General Functions for dealing with APM Data                                          #  
+#                                                                                        #
+##########################################################################################
+
+### Function to source all other necessary custom functions ------------------------------
+
+sourceAPMFunctions <- function(offline=FALSE,              # Are you offline?
+                               verbose=FALSE               # Show progress
+)
+{
+  
+  if(verbose) cat('Sourcing Custom APM Functions\n')
+  
+  if(offline){
+    source('c:/Code/research/ausPropMrkt/prrFunctions.R')
+    source('c:/Code/research/ausPropMrkt/apmDataOptions.R')
+    source('c:/Code/research/ausPropMrkt/apmDatPrepFunctions.R')
+  } else {
+    source(paste0('https://raw.githubusercontent.com/andykrause/ausPropMrkt/',
+                  'master/prrFunctions.R'))
+    source(paste0('https://raw.githubusercontent.com/andykrause/ausPropMrkt/',
+                  'master/apmDataOptions.R'))
+    source(paste0('https://raw.githubusercontent.com/andykrause/ausPropMrkt/',
+                  'master/apmDataPrepFunctions.R'))
+  }
+}

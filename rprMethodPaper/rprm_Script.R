cee91bac45c120188926f23677d87e139e772c45
Andy Krause <andy.krause@unimelb.edu.au>
2016-04-08 21:56:45 +1000

    code cleanup

--- rprMethodPaper/rprm_Script.R
+++ rprMethodPaper/rprm_Script.R
@@ -54,56 +54,46 @@
   
  } 
 
-
- 
- ## Fix the factor levels in the errors  
-   ## Load prepared data
-
-  if(verbose) cat('Loading cleaned data\n')
-  load(paste0(dataPath, 'cleanTrans.RData'))
-  load(paste0(dataPath, 'studyShps.RData'))
-
 ### Analyze Raw Data ---------------------------------------------------------------------  
   
  ## Do data analysis  
 
   if(reAnalyze){
+   
+   # Load cleaned data 
+   if(verbose) cat('Loading cleaned data\n')
+   load(paste0(dataPath, 'cleanTrans.RData'))
     
-    # Calculate full results
-    full.results <- apmFullDataAnalysis(clean.trans=cleanTrans,
-                                        data.path=dataPath,
-                                        writeout=TRUE)
+   # Calculate full results
+   apmFullDataAnalysis(trans.data=cleanTrans,
+                       data.path=dataPath,
+                       writeout=TRUE,
+                       return.values=FALSE)
     
-    # Strip values out and clean up
-    yield.data <- full.results$tidy.data
-    hedimp.data <- full.results$impute.data
-    match.data <- full.results$match.data
-    index.data <- full.results$index.values
-    results <- full.results$results
-    rm(full.results); gc()
-    
-  } else  {
-    
-    load(paste0(dataPath, 'yieldResults.RData'))
+  } 
 
-  }
-
+ ## Load analysis results      
+  
+  load(paste0(dataPath, 'yieldResults.RData'))
+  load(paste0(dataPath, 'studyShps.RData'))
+  
 ### Compare matched data to all data -----------------------------------------------------
   
   #if(verbose) cat('Swapping Impute and Matched Yields\n')
-  comp.samp <- apmCompareSamples(clean.trans)
+  comp.samp <- apmCompareSamples(trans.data)
   
-  ## TODO: So there are differences.  How to adjust??  
+ ### Need to look within each area as current distributions do not account for fact that 
+  # more renting occurs in central city
 
 ### Prep Data Visualization --------------------------------------------------------------  
   
  ## Set up graphics parameters 
   
-  apmPlotOption()
+  apmPlotOptions()
   
  ## Isolate the necessary data
-  
-  yield.data <- yield.data[yield.data$method != 'spag', ]
+
+  yield.data$method <- as.character(yield.data$method)
   yield.data$method[yield.data$method=='hedimp'] <- 'Impute'
   yield.data$method[yield.data$method=='srm'] <- 'Match'
   yield.data$method <- factor(yield.data$method, levels=c('Index', 'Impute', 'Match'))
@@ -195,211 +185,84 @@
     
   ggMultiPlots(sub.house, sub.unit) 
   
+### Comparisons of Differences in RPRs methods by geo.level ------------------------------
   
+ ## Prepare data  
   
+  # Extract the index values
+  glob.index <- index.data$Global$Global
   
+  # Build house appr rate changes
+  har <- c(0, ((glob.index$raw$house.sale[2:20] - glob.index$raw$house.sale[1:19])/
+                 glob.index$raw$house.sale[1:19]))
+  har <- lowess(har)$y
+  har <- data.frame(time=1:20, app.rate=har)
   
+  # Build unit appr rate changes
+  uar <- c(0, ((glob.index$raw$unit.sale[2:20] - glob.index$raw$unit.sale[1:19])/
+                 glob.index$raw$unit.sale[1:19]))
+  uar <- lowess(uar)$y
+  uar <- data.frame(time=1:20, app.rate=uar)
   
+  # Make difference data
+  dif.data <- calcDifGeoWrap(geo.data, har, uar, apmOptions$geo.levels)
   
+ ## Make plots of differences between methods versus appreciation rate --------------------
   
-  
-  
-  
-### Comparisons of Differences in RPRs methods by unit by geo.level ----------------------
-  
-  ## Isolate the data  
-  
-  calcDifWrap <- function(x.data){
-  h.data <- x.data[x.data$type == 'house', ]
-  u.data <- x.data[x.data$type == 'unit', ]
-  
-  h.dif <- calcDif(h.data)
-  u.dif <- calcDif(u.data)
-  
-  return(list(houses=h.dif,
-              units=u.dif))
-  
-}  
-    
-  calcDif <- function(x.data){
-  x.data$UID <- paste0(x.data$geo, "..", x.data$time)
-  x.split <- split(x.data, as.factor(x.data$method))
-  x.ind_mat <- merge(x.split$Index, x.split$Match[,c('UID', 'yield')], by='UID')
-  x.ind_imp <- merge(x.split$Index, x.split$Impute[,c('UID', 'yield')], by='UID')
-  x.imp_mat <- merge(x.split$Impute, x.split$Match[,c('UID', 'yield')], by='UID')
-  
-  x.ind_mat$meth.dif <- x.ind_mat$yield.x - x.ind_mat$yield.y
-  x.ind_mat$comp.method <- 'Index - Match'
-  
-  x.ind_imp$meth.dif <- x.ind_imp$yield.x - x.ind_imp$yield.y
-  x.ind_imp$comp.method <- 'Index - Impute'
-  
-  x.imp_mat$meth.dif <- x.imp_mat$yield.x - x.imp_mat$yield.y
-  x.imp_mat$comp.method <- 'Impute - Match'
-  
-  return(rbind(x.ind_mat, x.ind_imp, x.imp_mat))
-  
-}  
-
-  glob.dif <- calcDifWrap(geo.data$Global)
-  lga.dif <- calcDifWrap(geo.data$lga)
-  sub.dif <- calcDifWrap(geo.data$suburb)
-
-  # Make house plot
-  house.bias.plot.time <- ggplot(glob.dif$houses, 
-                                 aes(x=time, y=meth.dif)) + 
-    geom_point(colour='black', size=2) + 
+ # Make house plot
+   house.bias.plot.time <- ggplot(dif.data$houses, 
+                                 aes(x=appr.rate, y=meth.dif, colour=geo.level)) + 
+    geom_point(colour='black', size=.1, alpha=.1) + 
     geom_smooth(method=loess, size=2) +
-    geom_smooth(data=lga.dif$houses, aes(x=time, y=meth.dif), method=loess, size=2, color='red') +
-    geom_smooth(data=sub.dif$houses, aes(x=time, y=meth.dif), method=loess, size=2, color='green') +
     xlab("Time\n") +
     ylab("Rental Yield Bias from Matched Results\n") +
-    scale_x_continuous(limits=c(0, 20),
-                       breaks=seq(0, 20, 4),
-                       labels=2011:2016) +
+    scale_y_continuous(limits=c(-.008, 0.0005),
+                       breaks=seq(-.01, 0, .0025)) +
     facet_wrap(~comp.method) +
     theme_prr
   house.bias.plot.time
   
-  # Make house plot
-  unit.bias.plot.time <- ggplot(glob.dif$units, 
-                                 aes(x=time, y=meth.dif)) + 
-    geom_point(colour='black', size=2) + 
+ # Make unit plot
+  unit.bias.plot.time <- ggplot(dif.data$units, 
+                                 aes(x=appr.rate, y=meth.dif, colour=geo.level)) + 
+    geom_point(colour='black', size=.1, alpha=.1) + 
     geom_smooth(method=loess, size=2) +
-    geom_smooth(data=lga.dif$units, aes(x=time, y=meth.dif), method=loess, size=2, color='red') +
-    geom_smooth(data=sub.dif$units, aes(x=time, y=meth.dif), method=loess, size=2, color='green') +
-  
     xlab("Time\n") +
     ylab("Rental Yield Bias from Matched Results\n") +
-    scale_x_continuous(limits=c(0, 20),
-                       breaks=seq(0, 20, 4),
-                       labels=2011:2016) +
+    scale_y_continuous(limits=c(-.008, 0.0005),
+                       breaks=seq(-.01, 0, .0025)) +
     facet_wrap(~comp.method) +
     theme_prr
   unit.bias.plot.time
 
+### Density plot of differences in all vs matched ----------------------------------------
   
+  ggplot(trans.data[trans.data$transType == 'sale' & 
+                          trans.data$PropertyType == 'House',], aes(x=transValue)) +
+    geom_density()+
+    geom_density(data=match.data[match.data$PropertyType == 'House',],
+                 aes(x=saleValue), color='red') + 
+    scale_x_continuous(limits=c(400000, 1400000))+
+    facet_wrap(~lga)
   
+  a<-table(trans.data$lga[trans.data$transType == 'sale' & 
+                            trans.data$PropertyType == 'House'])  
+  b<-table(match.data$lga[match.data$PropertyType == 'House'])  
+  cc<-intersect(names(a),names(b))
   
+  a <- a[names(a) %in% cc]
+  b <- b[names(b) %in% cc]
   
-  
-  
-  
-  
+  d<- (b/a) / (nrow(match.data[match.data$PropertyType == 'House',]) / 
+                 nrow(trans.data[trans.data$transType == 'sale' & 
+                             trans.data$PropertyType == 'House',]))
+
+  studyShapes$lga@data$rentLQ <- d[match(studyShapes$lga@data$LGA_NAME11, names(d))]
     
   
-
-   
- ## Compare bias in method vs appreciation
-   
-   # Extract the index values
-   glob.values <- index.values$Global$Global
-   
-   # Build a house and unit index
-   har <- c(0, ((glob.values$raw$house.sale[2:20] - glob.values$raw$house.sale[1:19])/
-                  glob.values$raw$house.sale[1:19]))
-   har <- lowess(har)$y
-   uar <- c(0, ((glob.values$raw$unit.sale[2:20] - glob.values$raw$unit.sale[1:19])/
-                  glob.values$raw$unit.sale[1:19]))
-   uar <- lowess(uar)$y
-   
-   
+### Save full workspace  
  
-   # Make house plot
-   
-   house.bias.plot <- ggplot(globHH, 
-                             aes(x=time, y=Bias)) + 
-     geom_point(colour='blue', size=2) + 
-     geom_smooth(method=loess, se=TRUE, size=2) +
-     xlab("") +
-     ylab("RPR Difference from Match\n") +
-     scale_x_continuous(limits=c(1,20),
-                        breaks=c(4,12,20), 
-                        labels=c(2012, 2014, 2016)) +
-     scale_y_continuous(limits=c(-.01, 0.002),
-                        breaks=seq(-.01, 0, .002)) +
-     facet_wrap(~method) +
-     geom_hline(yintercept = 0) + 
-     theme_prr
-   
-   house.bias.plot
-   
-     # Unit plot
-   
-   unit.bias.plot <- ggplot(globUU, 
-                            aes(x=time, y=Bias)) + 
-     geom_point(colour='forestgreen', size=2) + 
-     geom_smooth(method=loess, se=TRUE, size=2, color='forestgreen') +
-     xlab("") +
-     ylab("RPR Difference from Match\n") +
-     scale_x_continuous(limits=c(1,20),
-                        breaks=c(4,12,20), 
-                        labels=c(2012, 2014, 2016)) +
-     scale_y_continuous(limits=c(-.01, 0.002),
-                        breaks=seq(-.01, 0, .002)) +
-     facet_wrap(~method) +
-     geom_hline(yintercept = 0) + 
-     theme_prr
-   
-   unit.bias.plot  
-
- ## Compare the biases over geo level
-  
-  # create data
-  
-  level.bias <- lapply(as.list(apmOptions$geo.level), 
-                       FUN=apmCalcBias, yield.data=yield.tidy)       
-  level.bias <- rbind.fill(level.bias)
-  level.bias$geo.level <- factor(level.bias$geo.level, levels = c('Global', 'lga',
-                                                            'sla1', 'postCode', 'suburb'))
-  
-  house.bias <- level.bias[level.bias$type == 'Houses',]
-  unit.bias <- level.bias[level.bias$type == 'Units',]
-  
-  # PLot house results
-  
-  house.all.diff <- ggplot(house.bias, 
-                           aes(x=time, y=Bias, group=geo.level, color=geo.level)) + 
-    geom_point(colour='black', size=.3, alpha=.1) + 
-    geom_smooth(method=loess, size=1, se=TRUE) +
-    xlab("\nHouses\n") +
-    ylab("Rent-Price Ratio Difference from Match\n") +
-    scale_colour_manual(values=c('black', 'darkred', 'red', 'orange', 'yellow')) +
-    
-    scale_x_continuous(limits=c(0, 20),
-                       breaks=seq(4, 20, 4),
-                       labels=2012:2016) +
-    scale_y_continuous(limits=c(-.01, 0.005),
-                       breaks=seq(-.01, .005, .0025)) +
-    
-    facet_wrap(~method) +
-    geom_hline(yintercept = 0) + 
-    theme_prr
-  house.all.diff
-  
-  
-  unit.all.diff <- ggplot(unit.bias, 
-                          aes(x=time, y=Bias, group=geo.level, color=geo.level)) + 
-    geom_point(colour='black', size=.3, alpha=.1) + 
-    geom_smooth(method=loess, size=1, se=TRUE) +
-    xlab("\nUnits\n") +
-    ylab("Rent-Price Ratio Difference from Match\n") +
-    scale_colour_manual(values=c('black', 'darkred', 'red', 'orange', 'yellow')) +
-    
-    scale_x_continuous(limits=c(0, 20),
-                       breaks=seq(4, 20, 4),
-                       labels=2012:2016) +
-    scale_y_continuous(limits=c(-.01, 0.005),
-                       breaks=seq(-.01, .005, .0025)) +
-    
-    facet_wrap(~method) +
-    geom_hline(yintercept = 0) + 
-    theme_prr
-  unit.all.diff
-  
- ### Save plotting information            
-
-   save.image(paste0(dataPath, 'aresWrkspce.RData'))
+  save.image(paste0(dataPath, 'rprWrkspce.RData'))
    
        
        
